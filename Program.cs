01fb5061f44e2531537909dec6acb1d3ad2b7410
Ree02 <ri.02kh.aax@gmail.com>
2022-10-29 21:17:15 +0900

    :beers: prテスト

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Twitterの情報を取得");
         var test = new Twitter();
         test.getTwitterInformation();
     }
