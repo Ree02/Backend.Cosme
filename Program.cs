34e12a917d8b4c325d713a9adabdc8e806c8739d
Ree02 <ri.02kh.aax@gmail.com>
2022-10-29 21:14:10 +0900

    :adhesive_bandage: コンソールログ削除

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Twitter");
         var test = new Twitter();
         test.getTwitterInformation();
     }
